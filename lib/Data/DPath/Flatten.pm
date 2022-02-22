560b226f2e3504b6635851cd2704352661828fcc
Robert Wohlfarth <robert.j.wohlfarth@vumc.org>
2022-02-22 09:46:48 -0600

    Removed "aliases"
    I was over thinking the problem when I added the aliases. They aren't
    necessary to flatten a record. Worse, they would duplicate data, wasting
    memory. Removed those and converted back to returning a hash reference.
    Much easier to deal with, conceptually.

--- lib/Data/DPath/Flatten.pm
+++ lib/Data/DPath/Flatten.pm
@@ -11,18 +11,18 @@ Data::DPath::Flatten - Convert complex data structure into key/value pairs
   my $hash = flatten( \%record );
   
   # Aliases add more human readable field names.
-  my $hash = flatten( \@record, A => '/*[0]', B => '/*[1]' );
-  my $hash = flatten( \%record, {A => '/*[0]', B => '/*[1]'} );
+  my $hash = flatten( \@record );
+  my $hash = flatten( \%record );
 
 =head1 DESCRIPTION
 
 B<Data::DPath::Flatten> transforms an arbitrary data structure into a hash of
 key/value pairs. 
 
-Why? L<ETL::Pipeline::Input> returns arbitrary data structures. For example, 
-Excel files return an array but XML files a hash. B<Data::DPath::Flatten> 
-converts both of these into a standardized structure. I can query these records 
-later based on the XML tags or column headers from Excel.
+Why? To store raw data in an SQL database. L<ETL::Pipeline::Input> returns 
+arbitrary data structures. For example, Excel files return an array but XML 
+files a hash. B<Data::DPath::Flatten> gives me a unique key for each field in
+the file, regardless of the Perl data structure.
 
 Use B<Data::DPath::Flatten> where you need key/value pairs from arbitrary data.
 The module traverses nested data structures of any depth and converts into a 
@@ -34,10 +34,7 @@ package Data::DPath::Flatten;
 
 use 5.14.0;
 use Carp;
-use Data::DPath qw/dpath/;
-use Data::Visitor::Callback;
 use Exporter qw/import/;
-use List::AllUtils qw/pairs/;
 
 
 our @EXPORT = (qw/flatten/);
@@ -46,17 +43,16 @@ our $VERSION = '1.00';
 
 =head1 FUNCTIONS
 
-=head3 flatten( $data[, @aliases] )
+=head3 flatten( $data )
 
 B<flatten> takes an arbitrary data structure and converts into a one level array
 reference. Essentially, it flattens out nested data structures.
 
-B<flatten> returns an array reference that can be processed as pairs. The first
-element in each pair is either an alias name or L<Data::DPath> path into the 
-original record. The second element is the value from the record.
+B<flatten> returns a hash reference. The keys are L<Data::DPath> paths into the 
+original record. The value is the raw data value from the file.
 
-The first parameter - C<$data> - is required. This is a reference to the input
-data structure.
+The parameter C<$data> is required. It is a reference to the input data 
+structure.
 
   # Recursively traverse arrays and hashes. 
   my $hash = flatten( \@fields );
@@ -66,23 +62,6 @@ data structure.
   my $hash = flatten( $single );
   my $hash = flatten( \$single );
 
-C<@aliases> are optional. This is an array, array reference, hash, or hash 
-reference of field names and L<Data::DPath> paths. B<flatten> retrieves the 
-values at each path and saves them with the field name. If a path returns more
-than one value, B<flatten> adds each value to the output with the same name. 
-
-  # In addition to all the DPaths, these also add keys for "One" and "Two".
-  my $list = flatten( \@record, One => '/*[0]', Two => '/*[1]' );
-  
-  # A hash reference of aliases works the same.
-  my $list = flatten( \@record, {One => '/*[0]', Two => '/*[1]'} );
-
-  # A list of aliases can repeat names. There will be two "One" entries in
-  # the output.
-  my $list = flatten( \@record, One => '/*[0]', One => '/*[1]' );
-
-=head4 References
-
 When B<flatten> encounters a HASH or ARRAY reference, it recursively traverses
 the nested structure. Circular references are traversed only once, to avoid 
 infinite loops.
@@ -91,16 +70,6 @@ SCALAR references are dereferenced and the value stored.
 
 All other references and objects are stored as references.
 
-=head4 Array vs. Hash
-
-Why an array instead of a hash? This allows aliases to resolve to more than one
-value. If you use file headers as aliases, there is a good chance of collisions.
-Using an array allows B<flatten> to associate the same alias name with more than
-one data value.
-
-This also prevents an alias from overwriting a value from the original record.
-They are both returned.
- 
 =cut
 
 sub flatten {
@@ -108,29 +77,8 @@ sub flatten {
 	
 	# Flatten the original data into a one level hash. Make sure I get a new
 	# reference for every call.
-	my $new  = [];
+	my $new  = {};
 	_step( $data, $new, '', {} );
-
-	# Add aliases. Data from files might have column headers. This way I can
-	# look up data by it's path or column name.
-	my @aliases;
-	if (scalar( @_ ) == 1) {
-		my $options = $_[0];
-		if (defined $options) {
-			if    (ref( $options ) eq 'ARRAY') { @aliases = @$options; }
-			elsif (ref( $options ) eq 'HASH' ) { @aliases = %$options; }
-			else { croak 'Aliases must be either an ARRAY reference or a HASH reference'; }
-		}
-	} elsif (scalar( @_ ) % 2) { croak 'Aliases ARRAY must have an even number of items'; }
-	else { @aliases = @_; }
-
-	foreach my $pair (pairs @aliases) {
-		my ($key, $value) = @$pair;
-		my @found = dpath( $value )->match( $data );
-
-		if (scalar( @found ) == 0) { push @$new, $key, undef; } 
-		else { push( @$new, $key, $_ ) foreach (@found); }
-	}
 
 	# Return the flattened hash reference.	
 	return $new;
@@ -157,11 +105,11 @@ sub _step {
 	if (!defined( $from )) { 
 		# No op!
 	} elsif (ref( $from ) eq '') { 
-		if ($path eq '') { push @$to, '/'  , $from; }
-		else             { push @$to, $path, $from; }
+		if ($path eq '') { $to->{'/'  } = $from; }
+		else             { $to->{$path} = $from; }
 	} elsif (ref( $from) eq 'SCALAR') { 
-		if ($path eq '') { push @$to, '/'  , $$from; }
-		else             { push @$to, $path, $$from; }
+		if ($path eq '') { $to->{'/'  } = $$from; }
+		else             { $to->{$path} = $$from; }
 	} elsif (ref( $from) eq 'HASH') {
 		unless (exists $seen->{$from}) {
 			$seen->{$from}++;
@@ -178,8 +126,8 @@ sub _step {
 			}
 		}
 	} else {
-		if ($path eq '') { push @$to, '/'  , $from; }
-		else             { push @$to, $path, $from; }
+		if ($path eq '') { $to->{'/'  } = $from; }
+		else             { $to->{$path} = $from; }
 	}
 
 	return;
--- t/Flatten.t
+++ t/Flatten.t
@@ -1,37 +1,16 @@
 use strict;
 use warnings;
 
-use Data::DPath qw/dpath/;
-use List::AllUtils qw/pairs pairmap/;
 use Test::More;
 
 
 BEGIN { use_ok( 'Data::DPath::Flatten', qw/flatten/ ); }
 
-my $got1 = flatten( {A => 1} );
-my $got2 = flatten( {A => 1}, undef );
-is_deeply( $got2, $got1, 'undef same as no aliases' );
-
 sub compare {
-	my ($test, $data, $expected, $aliases) = @_;
+	my ($test, $data, $expected) = @_;
 
-	subtest $test => sub {
-		my $got = flatten( $data, $aliases );
-		
-		sort_pairs( $expected );
-		sort_pairs( $got      );
-		is_deeply( $got, $expected, 'Flattened' );
-
-		unless (defined $aliases) {
-			foreach my $pair (pairs @$got) {
-				my ($path, $value) = @$pair;
-	
-				my @actual = dpath( $path )->match( $data );
-				is( $actual[0], $value, 'Valid path' );
-				is( scalar( @actual ), 1, 'Path is unique' );
-			}
-		}
-	};
+	my $got = flatten( $data );
+	is_deeply( $got, $expected, $test );
 }
 
 compare( 'Hash reference',
@@ -42,7 +21,7 @@ compare( 'Hash reference',
 		F => [5, 6],
 		G => [{H => 7}, {I => 8}],
 		J => {K => [9, {L => 10}]},
-	}, [
+	}, {
 		'/A'          => 1,
 		'/B'          => 2,
 		'/C/D'        => 3,
@@ -53,7 +32,7 @@ compare( 'Hash reference',
 		'/G/*[1]/I'   => 8,
 		'/J/K/*[0]'   => 9,
 		'/J/K/*[1]/L' => 10,
-	]
+	}
 );
 compare( 'Array reference',
 	[
@@ -63,7 +42,7 @@ compare( 'Array reference',
 		[5, 6],
 		[{E => 7}, {F => 8}],
 		{G => [9, {H => 10}]},
-	], [
+	], {
 		'/*[0]'          => 1,
 		'/*[1]/A'        => 2,
 		'/*[2]/B/C'      => 3,
@@ -74,143 +53,25 @@ compare( 'Array reference',
 		'/*[4]/*[1]/F'   => 8,
 		'/*[5]/G/*[0]'   => 9,
 		'/*[5]/G/*[1]/H' => 10,
-	]
-);
-compare( 'With alias array',
-	{
-		A => 1,
-		B => 2,
-		C => {D => 3, E => 4},
-		F => [5, 6],
-		G => [{H => 7}, {I => 8}],
-		J => {K => [9, {L => 10}]},
-	}, [
-		'/A'          => 1,
-		'/B'          => 2,
-		'/C/D'        => 3,
-		'/C/E'        => 4,
-		'/F/*[0]'     => 5,
-		'/F/*[1]'     => 6,
-		'/G/*[0]/H'   => 7,
-		'/G/*[1]/I'   => 8,
-		'/J/K/*[0]'   => 9,
-		'/J/K/*[1]/L' => 10,
-		One           => 2,
-		Two           => 8,
-		Three         => undef,
-	], [
-		One   => '/B',
-		Two   => '/G/*[1]/I',
-		Three => '/Z',
-	]
-);
-compare( 'With alias hash',
-	{
-		A => 1,
-		B => 2,
-		C => {D => 3, E => 4},
-		F => [5, 6],
-		G => [{H => 7}, {I => 8}],
-		J => {K => [9, {L => 10}]},
-	}, [
-		'/A'          => 1,
-		'/B'          => 2,
-		'/C/D'        => 3,
-		'/C/E'        => 4,
-		'/F/*[0]'     => 5,
-		'/F/*[1]'     => 6,
-		'/G/*[0]/H'   => 7,
-		'/G/*[1]/I'   => 8,
-		'/J/K/*[0]'   => 9,
-		'/J/K/*[1]/L' => 10,
-		One           => 2,
-		Two           => 8,
-		Three         => undef,
-	], {
-		One   => '/B',
-		Two   => '/G/*[1]/I',
-		Three => '/Z',
 	}
-);
-compare( 'Aliases with same name',
-	{
-		A => 1,
-		B => 2,
-		C => {D => 3, E => 4},
-		F => [5, 6],
-		G => [{H => 7}, {I => 8}],
-		J => {K => [9, {L => 10}]},
-	}, [
-		'/A'          => 1,
-		'/B'          => 2,
-		'/C/D'        => 3,
-		'/C/E'        => 4,
-		'/F/*[0]'     => 5,
-		'/F/*[1]'     => 6,
-		'/G/*[0]/H'   => 7,
-		'/G/*[1]/I'   => 8,
-		'/J/K/*[0]'   => 9,
-		'/J/K/*[1]/L' => 10,
-		One           => 1,
-		One           => 2,
-		Two           => 8,
-	], [
-		One => '/A',
-		One => '/B',
-		Two => '/G/*[1]/I',
-	]
-);
-compare( 'Aliases overwrites field',
-	{
-		A => 1,
-		B => 2,
-		C => {D => 3, E => 4},
-		F => [5, 6],
-		G => [{H => 7}, {I => 8}],
-		J => {K => [9, {L => 10}]},
-	}, [
-		'/A'          => 1,
-		'/A'          => 3,
-		'/B'          => 2,
-		'/C/D'        => 3,
-		'/C/E'        => 4,
-		'/F/*[0]'     => 5,
-		'/F/*[1]'     => 6,
-		'/G/*[0]/H'   => 7,
-		'/G/*[1]/I'   => 8,
-		'/J/K/*[0]'   => 9,
-		'/J/K/*[1]/L' => 10,
-	], ['/A' => '/C/D']
 );
 
 subtest 'Infinite loop' => sub {
 	my (@a, @b);
 	@a = (1, \@b);
 	@b = (2, \@a);
-	compare( 'Array', \@a, [
+	compare( 'Array', \@a, {
 		'/*[0]'      => 1,
 		'/*[1]/*[0]' => 2,
-	] );
+	} );
 
 	my (%a, %b);
 	%a = (A => 1, B => \%b);
 	%b = (C => 2, D => \%a);
-	compare( 'Hash', \%a, [
+	compare( 'Hash', \%a, {
 		'/A'   => 1,
 		'/B/C' => 2,
-	] );
+	} );
 };
 
 done_testing();
-
-
-sub sort_pairs {
-	my $list = shift;
-	
-	my @work = pairmap { [$a, $b] } @$list;
-	@work = sort {
-		$a->[0] cmp $b->[0]
-		|| $a->[1] cmp $b->[1]
-	} @work;
-	@$list = map { ($_->[0], $_->[1]) } @work;
-}
